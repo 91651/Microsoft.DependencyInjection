25da4c949e13bcd1a3e9b5795e78d567037773a0
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2018-08-29 15:01:05 +0800

    修改项目名称

--- F.DependencyInjection.Extensions/BuilderExtensions.cs
+++ F.DependencyInjection.Extensions/BuilderExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.DependencyInjection.Extensions
+namespace F.DependencyInjection.Extensions
 {
     public static class BuilderExtensions
     {
--- Sample/Startup.cs
+++ Sample/Startup.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using F.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.DependencyInjection.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
